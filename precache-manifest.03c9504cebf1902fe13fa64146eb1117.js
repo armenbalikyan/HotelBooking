55d5076367125f79cec7ea2dee93c92a121324e1
armenbalikyan <armbalikyans@gmail.com>
2020-02-21 14:48:41 +0400

    Updates

---
+++ precache-manifest.03c9504cebf1902fe13fa64146eb1117.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb29cd59ea1282bf990286dcfcd572db",
+    "url": "/HotelBooking/index.html"
+  },
+  {
+    "revision": "eb955292c718d7ce659a",
+    "url": "/HotelBooking/static/css/main.15bff03e.chunk.css"
+  },
+  {
+    "revision": "d241050e5a6fc288e8e6",
+    "url": "/HotelBooking/static/js/2.f3614e0a.chunk.js"
+  },
+  {
+    "revision": "84a2a7a390df1ac99fa4fadf23155e60",
+    "url": "/HotelBooking/static/js/2.f3614e0a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "eb955292c718d7ce659a",
+    "url": "/HotelBooking/static/js/main.33b90e1c.chunk.js"
+  },
+  {
+    "revision": "538e808b88b1f1f3c608",
+    "url": "/HotelBooking/static/js/runtime-main.4be1f377.js"
+  }
+]);
